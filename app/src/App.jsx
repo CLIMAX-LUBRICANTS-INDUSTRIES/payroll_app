ba49e7a73586339ba520b413a679d86cfee23e19
Isaac Johnson <ilaaalm@yahoo.com>
2026-05-13 14:51:18 +0100

    - changed bg image - added <main> - inital table

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,15 @@
+import bg from './assets/bg2.jpg';
 import Layout from "./layout/Layout.jsx";
 
 function App() {
   return(
-    <div>
+    <div 
+    className='backdrop-blur'
+    style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
       <Layout />
     </div>
   )
